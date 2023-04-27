5416d7c1c3b5211548e837426e1b2a9d4c98290d
mkarsums15@mail.ru <mkarsums15@mail.ru>
2023-04-27 18:37:58 +0300

    add solution

--- linear-search.js
+++ linear-search.js
@@ -2,16 +2,31 @@
 //принимает два аргумента: объект и массив. Она должна последовательно перебирать каждый элемент и возвращать индекс,
 // в котором объект был изначально найден. Если объект не найден, то функция возвращает значение -1.
 function linearSearch(n, arr) {
-
+	let index = -1;
+	for (let i = arr.length - 1; i >= 0; i--) {
+		if (arr[i] === n) {
+			index = i;
+		}
+	}
+	return index;
 }
 
+console.log(linearSearch(5, [3, 7, 5, 8, 5]));
 
 //напишем новый метод globalLinearSearch, который возвращает массив всех индексов, в которых найден элемент.
 function globalLinearSearch(n, arr) {
-
+	let indexArr = [];
+	arr.forEach((el, index) => {
+		if (el === n) {
+			indexArr.push(index);
+		}
+	});
+	return indexArr;
 }
+
+console.log(globalLinearSearch("a", ["b", "a", "n", "a", "n", "a", "s"]));
 
 module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
+	linearSearch,
+	globalLinearSearch,
+};
--- spec/linear-search.spec.js
+++ spec/linear-search.spec.js
@@ -1,17 +1,57 @@
-const { linearSearch, globalLinearSearch } = require('../linear-search');
+const { linearSearch, globalLinearSearch } = require("../linear-search");
 
-describe("linearSearch()", function() {
-  it("returns first index of item in array", function() {
-    let item = 3;
-    let arr = [1,2,3,4,5];
-    expect(linearSearch(item, arr)).toEqual(2);
-  });
+describe("linearSearch()", function () {
+	it("returns first index of item in array", function () {
+		let item = 3;
+		let arr = [1, 2, 3, 4, 5];
+		expect(linearSearch(item, arr)).toEqual(2);
+	});
+	it("returns first index of item in array", function () {
+		let item = 10;
+		let arr = [1, 2, 3, 4, 5];
+		expect(linearSearch(item, arr)).toBe(-1);
+	});
+	it("returns first index of item in array", function () {
+		let item = 3;
+		let arr = [1, 2, 3, 3, 3];
+		expect(linearSearch(item, arr)).toEqual(2);
+	});
+	it("returns first index of item in array", function () {
+		let item = -5;
+		let arr = [1, -5, 3, 4, 5];
+		expect(linearSearch(item, arr)).toEqual(1);
+	});
+	it("returns first index of item in array", function () {
+		let item = "a";
+		let arr = ["b", "a", "n", "a"];
+		expect(linearSearch(item, arr)).toBe(1);
+	});
+	it("returns first index of item in array", function () {
+		let item = "a";
+		let arr = ["b", "c", "n", "d"];
+		expect(linearSearch(item, arr)).toEqual(-1);
+	});
 });
 
-describe("globalLinearSearch()", function() {
-  it("returns array indxes of items in array", function() {
-    let item = 'a';
-    let arr = ['b', 'a', 'n', 'a', 'n', 'a', 's'];
-    expect(globalLinearSearch(item, arr)).toEqual([1, 3, 5]);
-  });
+describe("globalLinearSearch()", function () {
+	it("returns array indexes of items in array", function () {
+		let item = "a";
+		let arr = ["b", "a", "n", "a", "n", "a", "s"];
+		expect(globalLinearSearch(item, arr)).toEqual([1, 3, 5]);
+	});
+	it("returns array indexes of items in array", function () {
+		let item = "a";
+		let arr = ["b", "c", "n", "c", "n", "c", "s"];
+		expect(globalLinearSearch(item, arr)).toEqual([]);
+	});
+	it("returns array indexes of items in array", function () {
+		let item = 3;
+		let arr = [1, 2, 3, 3, 3];
+		expect(globalLinearSearch(item, arr)).toEqual([2, 3, 4]);
+	});
+	it("returns array indexes of items in array", function () {
+		let item = 3;
+		let arr = [1, 2, 4, 5, 6];
+		expect(globalLinearSearch(item, arr)).toEqual([]);
+	});
 });
